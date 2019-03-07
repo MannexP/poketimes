8ecd862933d6b6e91c5cd5a82e902da156613b08
Mannex Pimentel <Mannex@mannexs-air.attlocal.net>
2019-03-07 17:46:02 -0500

    nav

---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from 'React'
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, esse maiores. Aliquam repellendus reiciendis, nobis voluptate eos, sint ducimus, repudiandae culpa magnam inventore facilis pariatur aliquid delectus totam distinctio maxime.</p>
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'React'
+
+const Home = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, esse maiores. Aliquam repellendus reiciendis, nobis voluptate eos, sint ducimus, repudiandae culpa magnam inventore facilis pariatur aliquid delectus totam distinctio maxime.</p>
+        </div>
+    )
+}
+
+export default Home
